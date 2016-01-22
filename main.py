1a504ee153a85871b95274920f555b11fef8a034
Nico-Seiya <Nico-Seiya@users.noreply.github.com>
2016-01-22 23:29:57 +0100

    Update main.py

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ addon_handle = int(sys.argv[1])
 xbmcplugin.setContent(addon_handle, 'movies')
 
 url = 'http://www-igm.univ-mlv.fr/~badis/IR3/Streaming/backwaters.mpeg'
-li = xbmcgui.ListItem('My First Video!', iconImage='DefaultVideo.png')
+li = xbmcgui.ListItem('BlackWaters from TP-Streaming!', iconImage='DefaultVideo.png')
 xbmcplugin.addDirectoryItem(handle=addon_handle, url=url, listitem=li)
 
 xbmcplugin.endOfDirectory(addon_handle)
